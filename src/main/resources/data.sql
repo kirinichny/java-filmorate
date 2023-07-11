32a565340aa024fe4ebb8ab35238f806bff754d8
Kirin <kirin-89@yandex.ru>
2023-07-12 05:24:39 +1000

    feat: добавлен функционал согласно ТЗ 11

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,15 +1,33 @@
-INSERT INTO mpa_ratings (id, name) VALUES (1, 'G');
-INSERT INTO mpa_ratings (id, name) VALUES (2, 'PG');
-INSERT INTO mpa_ratings (id, name) VALUES (3, 'PG-13');
-INSERT INTO mpa_ratings (id, name) VALUES (4, 'R');
-INSERT INTO mpa_ratings (id, name) VALUES (5, 'NC-17');
+MERGE INTO mpa_ratings
+    USING (VALUES (1, 'G'),
+                  (2, 'PG'),
+                  (3, 'PG-13'),
+                  (4, 'R'),
+                  (5, 'NC-17')
+        ) AS new_ratings (id, name)
+ON mpa_ratings.id = new_ratings.id
+WHEN MATCHED THEN
+    UPDATE
+    SET name = new_ratings.name
+WHEN NOT MATCHED THEN
+    INSERT (id, name)
+    VALUES (new_ratings.id, new_ratings.name);
 
-INSERT INTO genres (id, name) VALUES (1, 'Комедия');
-INSERT INTO genres (id, name) VALUES (2, 'Драма');
-INSERT INTO genres (id, name) VALUES (3, 'Мультфильм');
-INSERT INTO genres (id, name) VALUES (4, 'Триллер');
-INSERT INTO genres (id, name) VALUES (5, 'Документальный');
-INSERT INTO genres (id, name) VALUES (6, 'Боевик');
+MERGE INTO genres
+    USING (VALUES (1, 'Комедия'),
+                  (2, 'Драма'),
+                  (3, 'Мультфильм'),
+                  (4, 'Триллер'),
+                  (5, 'Документальный'),
+                  (6, 'Боевик')
+        ) AS new_genres (id, name)
+ON genres.id = new_genres.id
+WHEN MATCHED THEN
+    UPDATE
+    SET name = new_genres.name
+WHEN NOT MATCHED THEN
+    INSERT (id, name)
+    VALUES (new_genres.id, new_genres.name);
 
 //INSERT INTO films ("id", "name", "description", "release_date", "duration_minutes", "mpa_rating_id") VALUES (1, 'Как работает Java', 'Документальный фильм, который погружает зрителя в удивительный мир программирования на языке Java.', '2022-08-02', 120, 1);
 //INSERT INTO films ("id", "name", "description", "release_date", "duration_minutes", "mpa_rating_id") VALUES (2, 'Интересное программирование', '"Интересное программирование" от Яндекс Практикум: Молодой программист погружается в мир таинственных багов и зловещих алгоритмов, где каждая строка кода может привести к смертельным последствиям.', '2023-07-08', 260, 5);
